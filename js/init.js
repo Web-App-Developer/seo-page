b86b6cfa9d596565344cca553e426143a1421f55
Web-App-Developer <lionking921117@gmail.com>
2021-01-15 13:53:58 -0600

    1/15 commit

--- js/init.js
+++ js/init.js
@@ -1,8 +1,14 @@
 function myFunction() {
 	var x = document.getElementById("myLinks");
-	if (x.style.display === "block") {
-	  x.style.display = "none";
-	} else {
-	  x.style.display = "block";
+	var y = document.getElementById("m-drop");
+	// if (x.style.display === "block") {
+	//   x.style.display = "none";
+	// } else {
+	//   x.style.display = "block";
+	// }
+	if (y.style.display==="block"){
+		y.style.display = "none";
+	}else{
+		y.style.display = "block";
 	}
 }
