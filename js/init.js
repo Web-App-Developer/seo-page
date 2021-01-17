b50aa527d37f6ffd4ff3bd585c69714f9668333a
Web-App-Developer <lionking921117@gmail.com>
2021-01-17 02:53:16 -0600

    svg - finish

--- js/init.js
+++ js/init.js
@@ -12,3 +12,12 @@ function myFunction() {
 		y.style.display = "block";
 	}
 }
+var parent = document.querySelector('.m-nav');
+var children = parent.children;
+for (var i = 0; i < children.length; i++) {
+	children[i].addEventListener("click", function() {
+	var current = document.getElementsByClassName(" active");
+	current[0].className = current[0].className.replace(" active", "");
+	this.className += " active";
+	});
+}
