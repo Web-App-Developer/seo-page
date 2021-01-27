0e5037aedc4d291a5a3a3bd34ee0b5c31d07b6b6
Web-App-Developer <lionking921117@gmail.com>
2021-01-28 03:25:29 +0800

    375px fix

--- js/init.js
+++ js/init.js
@@ -1,11 +1,6 @@
 function myFunction() {
     var x = document.getElementById("myLinks");
     var y = document.getElementById("m-drop");
-    // if (x.style.display === "block") {
-    //   x.style.display = "none";
-    // } else {
-    //   x.style.display = "block";
-    // }
     if (y.style.display === "block") {
         y.style.display = "none";
     } else {
